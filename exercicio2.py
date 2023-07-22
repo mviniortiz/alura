1e4bf69f1f17397ce71d6c9d827e367e7dc3c01a
Markus Vinicius <85716855+mviniortiz@users.noreply.github.com>
2023-07-21 23:47:10 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,8 @@
+#O exercício a seguir pede para que eu crie uma dada lista e escolha um número aleatoriamente desta lista
+
+from random import choice
+
+lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+
+lista_total = choice(lista)
+
